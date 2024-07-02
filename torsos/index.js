eac8cb53dcb2a59aacad2571f9db1781d1402ca5
stuyk <trevorwessel@protonmail.com>
2024-07-02 13:53:45 -0600

    fix: proprty name

--- torsos/index.js
+++ torsos/index.js
@@ -29,102 +29,102 @@ function processNames(files) {
 
 const male = {
   0: [
-    { dlc: "mp_m_0", drawable: 0 },
+    { dlcName: "mp_m_0", drawable: 0 },
     ...processNames(glob.sync("./male/0/*.png")),
   ],
   1: [
-    { dlc: "mp_m_0", drawable: 1 },
+    { dlcName: "mp_m_0", drawable: 1 },
     ...processNames(glob.sync("./male/1/*.png")),
   ],
   2: [
-    { dlc: "mp_m_0", drawable: 2 },
+    { dlcName: "mp_m_0", drawable: 2 },
     ...processNames(glob.sync("./male/2/*.png")),
   ],
   4: [
-    { dlc: "mp_m_0", drawable: 4 },
+    { dlcName: "mp_m_0", drawable: 4 },
     ...processNames(glob.sync("./male/4/*.png")),
   ],
   5: [
-    { dlc: "mp_m_0", drawable: 5 },
+    { dlcName: "mp_m_0", drawable: 5 },
     ...processNames(glob.sync("./male/5/*.png")),
   ],
   6: [
-    { dlc: "mp_m_0", drawable: 6 },
+    { dlcName: "mp_m_0", drawable: 6 },
     ...processNames(glob.sync("./male/6/*.png")),
   ],
   8: [
-    { dlc: "mp_m_0", drawable: 8 },
+    { dlcName: "mp_m_0", drawable: 8 },
     ...processNames(glob.sync("./male/8/*.png")),
   ],
   11: [
-    { dlc: "mp_m_0", drawable: 11 },
+    { dlcName: "mp_m_0", drawable: 11 },
     ...processNames(glob.sync("./male/11/*.png")),
   ],
   12: [
-    { dlc: "mp_m_0", drawable: 12 },
+    { dlcName: "mp_m_0", drawable: 12 },
     ...processNames(glob.sync("./male/12/*.png")),
   ],
   14: [
-    { dlc: "mp_m_0", drawable: 14 },
+    { dlcName: "mp_m_0", drawable: 14 },
     ...processNames(glob.sync("./male/14/*.png")),
   ],
   15: [
-    { dlc: "mp_m_0", drawable: 15 },
+    { dlcName: "mp_m_0", drawable: 15 },
     ...processNames(glob.sync("./male/15/*.png")),
   ],
 };
 
 const female = {
   0: [
-    { dlc: "mp_m_0", drawable: 0 },
+    { dlcName: "mp_m_0", drawable: 0 },
     ...processNames(glob.sync("./female/0/*.png")),
   ],
   1: [
-    { dlc: "mp_m_0", drawable: 1 },
+    { dlcName: "mp_m_0", drawable: 1 },
     ...processNames(glob.sync("./female/1/*.png")),
   ],
   2: [
-    { dlc: "mp_m_0", drawable: 2 },
+    { dlcName: "mp_m_0", drawable: 2 },
     ...processNames(glob.sync("./female/2/*.png")),
   ],
   3: [
-    { dlc: "mp_m_0", drawable: 3 },
+    { dlcName: "mp_m_0", drawable: 3 },
     ...processNames(glob.sync("./female/3/*.png")),
   ],
   4: [
-    { dlc: "mp_m_0", drawable: 4 },
+    { dlcName: "mp_m_0", drawable: 4 },
     ...processNames(glob.sync("./female/4/*.png")),
   ],
   5: [
-    { dlc: "mp_m_0", drawable: 5 },
+    { dlcName: "mp_m_0", drawable: 5 },
     ...processNames(glob.sync("./female/5/*.png")),
   ],
   6: [
-    { dlc: "mp_m_0", drawable: 6 },
+    { dlcName: "mp_m_0", drawable: 6 },
     ...processNames(glob.sync("./female/6/*.png")),
   ],
   7: [
-    { dlc: "mp_m_0", drawable: 7 },
+    { dlcName: "mp_m_0", drawable: 7 },
     ...processNames(glob.sync("./female/7/*.png")),
   ],
   9: [
-    { dlc: "mp_m_0", drawable: 9 },
+    { dlcName: "mp_m_0", drawable: 9 },
     ...processNames(glob.sync("./female/9/*.png")),
   ],
   11: [
-    { dlc: "mp_m_0", drawable: 11 },
+    { dlcName: "mp_m_0", drawable: 11 },
     ...processNames(glob.sync("./female/11/*.png")),
   ],
   12: [
-    { dlc: "mp_m_0", drawable: 12 },
+    { dlcName: "mp_m_0", drawable: 12 },
     ...processNames(glob.sync("./female/12/*.png")),
   ],
   14: [
-    { dlc: "mp_m_0", drawable: 14 },
+    { dlcName: "mp_m_0", drawable: 14 },
     ...processNames(glob.sync("./female/14/*.png")),
   ],
   15: [
-    { dlc: "mp_m_0", drawable: 15 },
+    { dlcName: "mp_m_0", drawable: 15 },
     ...processNames(glob.sync("./female/15/*.png")),
   ],
 };
